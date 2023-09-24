5098387acaec76652d214f700caaf1124428ba04
yosrajalali <100087227+yosrajalali@users.noreply.github.com>
2023-09-25 00:05:27 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Router>
+    <App />
+  </Router>
 );
