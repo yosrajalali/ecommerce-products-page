fdcfed63f9d9f011295c58ee4036608b169684a5
yosrajalali <100087227+yosrajalali@users.noreply.github.com>
2023-09-25 19:37:19 +0330

    update package.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import './app.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
 import Nav from './pages/Nav';
 import Home from './pages/Home';
-// import About from './pages/About';
-// import Contact from './pages/Contact';
-// import Men from './pages/Men';
-// import Women from './pages/Women';
-// import Collections from './pages/Collections';
 import { CartContextProvider } from './CartContext';
 
 function App() {
   return (
     <CartContextProvider>
-      <div className="container">
-        <Nav />
-        <BrowserRouter basename="ecommerce-products-page">
+      <HashRouter>
+        <div className="container">
+          <Nav />
           <Routes>
             <Route path="/" element={<Home />} />{' '}
           </Routes>{' '}
-        </BrowserRouter>
-      </div>{' '}
+        </div>{' '}
+      </HashRouter>{' '}
     </CartContextProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
